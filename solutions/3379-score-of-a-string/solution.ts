ddefc50ae94314464df1c2db2716f70e57e8cb64
Vítor França <vitor.willer@gmail.com>
2024-09-10 10:06:49 +0000

    [LeetCode Sync] Runtime - 60 ms (81.88%), Memory - 52.1 MB (82.08%)

---
+++ solutions/3379-score-of-a-string/solution.ts
@@ -0,0 +1,7 @@
+function scoreOfString(s: string): number {
+    let sum = 0;
+    for(let i = 0; i < s.length -1; i++) {
+        sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1))
+    }
+    return sum;
+};
